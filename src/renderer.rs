b897be163b30f994cf98a925dbb9f832edf1106a
yrncollo <ngenocollins599@gmail.com>
2026-05-08 11:35:11 +1000

    created unsafe renderer function

--- src/renderer.rs
+++ src/renderer.rs
@@ -4,3 +4,24 @@
 use anyhow::anyhow;
 use gl::types::*;
 use std::ffi::CString;
+
+pub unsafe fn compile_shader(src: &str, kind: GLenum) -> anyhow::Result<GLuint> {
+    let id = gl::CreateShader(kind);
+    let c_src = CString::new(src).unwrap();
+    let ptr = c_src.as_ptr();
+    gl::ShaderSource(id, 1, &ptr, std::ptr::null());
+    gl::CompileShader(id);
+
+    let mut ok: GLint = 0;
+    gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut ok);
+    if ok == 0 {
+        let mut len: GLint = 0;
+        gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+        let mut buf = vec![0u8; len as usize];
+        gl::GetShaderInfoLog(id, len, std::ptr::null_mut(), buf.as_mut_ptr() as *mut i8);
+        gl::DeleteShader(id);
+        let msg = String::from_utf8_lossy(&buf).into_owned();
+        return Err(anyhow!("Shader compile error:\n{}", msg));
+    }
+    Ok(id)
+}
