bde204c14c1c8586e4526ab7fa1cf6284ee7f76f
yrncollo <ngenocollins599@gmail.com>
2026-05-08 11:33:23 +1000

    created input struct

--- src/input.rs
+++ src/input.rs
@@ -3,3 +3,13 @@
 
 use crate::config::Config;
 use crate::math::Vec2;
+
+const VELOCITY_THRESHOLD: f32 = 15.0;
+
+// Mouse
+#[derive(Debug, Clone, Default)]
+pub struct Mouse {
+    pub curr: Vec2,
+    pub prev: Vec2,
+    pub drag: bool,
+}
