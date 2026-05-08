edc0cc7ff4fb2b6718df9d81384ffca07e971e4a
yrncollo <ngenocollins599@gmail.com>
2026-05-08 11:37:00 +1000

    used libGL to force the linker to use it

--- src/window.rs
+++ src/window.rs
@@ -4,3 +4,11 @@
 #[link(name = "GL")]
 #[link(name = "GLX")]
 extern "C" {}
+
+use anyhow::anyhow;
+use std::ffi::CString;
+use std::ptr;
+use x11::glx::{
+    glXChooseVisual, glXCreateContext, glXMakeCurrent, glXQueryVersion,
+    glXSwapBuffers, GLX_DEPTH_SIZE, GLX_DOUBLEBUFFER, GLX_RGBA,
+};
