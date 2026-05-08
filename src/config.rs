7f70fdd7186f9f9ec534de9fb6daba8b278fee5d
yrncollo <ngenocollins599@gmail.com>
2026-05-08 11:32:51 +1000

    added config struct

--- src/config.rs
+++ src/config.rs
@@ -2,3 +2,11 @@
 
 use std::fs;
 use std::path::Path;
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub min_scale: f32,
+    pub scroll_speed: f32,
+    pub drag_friction: f32,
+    pub scale_friction: f32,
+}
