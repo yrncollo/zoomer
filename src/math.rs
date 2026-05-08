0f5214205ea6ed121e53c4b5a9746da56751eb8f
yrncollo <ngenocollins599@gmail.com>
2026-05-08 11:34:16 +1000

    created vec2 struct

--- src/math.rs
+++ src/math.rs
@@ -2,3 +2,9 @@
 // Ported from boomer's la.nim and navigation.nim
 
 use std::ops::{Add, AddAssign, Div, Mul, Neg, Sub, SubAssign};
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
